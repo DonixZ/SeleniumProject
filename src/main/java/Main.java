b9161460330ae7d02aacc511e14fa841689230ee
DonixZ <dannyzaletsky@gmail.com>
2022-03-30 20:54:00 +0300

    Automating Amazon adding to cart and going to check out.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
@@ -11,13 +11,22 @@ public class Main {
         WebDriver ChromeTest = new ChromeDriver();
 
         ChromeTest.manage().window().maximize();
-        ChromeTest.get("https://google.co.il"); // Opens google site
-        try {
-            Thread.sleep(2500); // Wait for 2.5 seconds
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         ChromeTest.get("https://amazon.com"); // Opens amazon site
+
+        WebElement SearchTextBox = ChromeTest.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")); // Search field
+        SearchTextBox.sendKeys("duck car swing"); // Input value inside search field
+
+        WebElement ClickSearch = ChromeTest.findElement(By.xpath("//*[@id=\"nav-search-submit-button\"]")); // Search button
+        ClickSearch.click(); // Click on search button
+
+        ChromeTest.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div[1]/div/span[3]/div[2]/div[5]/div/div/div/div/div/div[2]/div/div/div[1]/h2/a/span")).click(); // Click on item after searching
+
+        WebElement AddToCart = ChromeTest.findElement(By.xpath("//*[@id=\"add-to-cart-button\"]")); // Cart button
+        AddToCart.click(); // Click Cart button
+
+        ChromeTest.findElement(By.name("proceedToRetailCheckout")).click(); // Click on Checkout
+
+
 
 
     }
