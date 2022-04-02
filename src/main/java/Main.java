a2747e1bdaadafbc5e45b9119ddc0e2ce532d060
DonixZ <dannyzaletsky@gmail.com>
2022-04-03 00:57:13 +0300

    Automation Project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,204 +11,45 @@ public class Main {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\danny\\Desktop\\Sel\\chromedriver.exe");
         WebDriver ChromeTest = new ChromeDriver();
 
-        // Opens the site
-        ChromeTest.manage().window().maximize();
-        ChromeTest.get("https://automationpractice.com/index.php");
-
-        // Find the login button and clicks on it
-        WebElement SignInButton = ChromeTest.findElement(By.className("login"));
-        SignInButton.click();
+        ChromeTest.manage().window().maximize(); // Maximizes windows size
+        ChromeTest.get("https://automationpractice.com/index.php"); // enters the site
+        ChromeTest.findElement(By.className("login")).click(); // clicks on login
+        ChromeTest.findElement(By.id("email_create")).sendKeys("Dannyzaletsky@gmail.com"); // sends value to email box
+        ChromeTest.findElement(By.id("SubmitCreate")).click(); // clicks on create account
 
         try {
-            Thread.sleep(3000);
+            Thread.sleep(2500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        // Find the email typing field, clears it and send value
-        WebElement EmailBox = ChromeTest.findElement(By.id("email_create"));
-        EmailBox.click();
-        EmailBox.sendKeys("Dannyzaletsky@gmail.com");
+        ChromeTest.findElement(By.id("id_gender1")).click(); // checks the Mr option
+        ChromeTest.findElement(By.id("customer_firstname")).sendKeys("Daniel"); // sends value to first name box
+        ChromeTest.findElement(By.id("customer_lastname")).sendKeys("Zaletsky"); // sends value to last name box
+        ChromeTest.findElement(By.id("passwd")).sendKeys("9Y!vW@HRd7jUmt7"); // sends value to password box
+        Select DaysBirthday = new Select(ChromeTest.findElement(By.id("days")));  // sets Days value to 18
+        DaysBirthday.selectByIndex(18); // sets Days value to 18
+        Select MonthsBirhday = new Select(ChromeTest.findElement(By.id("months"))); // sets Months value to 3
+        MonthsBirhday.selectByIndex(3); // sets Months value to 3
+        Select YearsBirhday = new Select(ChromeTest.findElement(By.id("years"))); // sets Years value to 1996
+        YearsBirhday.selectByValue(String.valueOf(1996)); // sets Years value to 1996
+        ChromeTest.findElement(By.id("address1")).sendKeys("Mivtza Yoav 35"); // sends value to address box
+        ChromeTest.findElement(By.id("city")).sendKeys("Beer Sheva"); // sends value to city box
+        Select State = new Select(ChromeTest.findElement(By.id("id_state"))); // sets State value to 1 alabama
+        State.selectByValue("1"); // sets State value to 1 alabama
+        ChromeTest.findElement(By.id("postcode")).sendKeys("35242"); // sends value to zipcode box
+        ChromeTest.findElement(By.id("phone_mobile")).sendKeys("0535261996"); // sends value to mobile phone box
+        WebElement AddressAlias = ChromeTest.findElement(By.id("alias")); // clears and then sends value to Alias Address box
+        AddressAlias.click(); // clears and then sends value to Alias Address box
+        AddressAlias.clear(); // clears and then sends value to Alias Address box
+        AddressAlias.sendKeys("Bobos"); // clears and then sends value to Alias Address box
 
         try {
-            Thread.sleep(3000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        // Find the Create Account button and clicks on it
-        WebElement CreateAccButton = ChromeTest.findElement(By.id("SubmitCreate"));
-        CreateAccButton.click();
-
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Finds the Mr check button and clicks on it
-        WebElement IdGender = ChromeTest.findElement(By.id("id_gender1"));
-        IdGender.click();
-
-        try {
-            Thread.sleep(1500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Finds the First name field and sends value
-        WebElement FirstName = ChromeTest.findElement(By.id("customer_firstname"));
-        FirstName.click();
-        FirstName.sendKeys("Daniel");
-
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Find the last name field and sends value
-        WebElement LastName = ChromeTest.findElement(By.id("customer_lastname"));
-        LastName.click();
-        LastName.sendKeys("Zaletsky");
-
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Finds the password field and sends value
-        WebElement PasswordSignUp = ChromeTest.findElement(By.id("passwd"));
-        PasswordSignUp.click();
-        PasswordSignUp.clear();
-        PasswordSignUp.sendKeys("9Y!vW@HRd7jUmt7");
-
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Finds the days drop down box and selects value
-        Select DaysBirthday = new Select(ChromeTest.findElement(By.id("days")));
-        DaysBirthday.selectByIndex(18);
-
-        try {
-            Thread.sleep(1200);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Finds the months drop down box and selects value
-        Select MonthsBirhday = new Select(ChromeTest.findElement(By.id("months")));
-        MonthsBirhday.selectByIndex(3);
-
-        try {
-            Thread.sleep(1200);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Finds the years drop down box and selects value
-        Select YearsBirhday = new Select(ChromeTest.findElement(By.id("years")));
-        YearsBirhday.selectByValue(String.valueOf(1996));
-
-        try {
-            Thread.sleep(1200);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Finds the adress box and sends value
-        WebElement Address = ChromeTest.findElement(By.id("address1"));
-        Address.click();
-        Address.sendKeys("Mivtza Yoav 35");
-
-        try {
-            Thread.sleep(1200);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Find the city box and sends value
-        WebElement City = ChromeTest.findElement(By.id("city"));
-        City.click();
-        City.sendKeys("Beer Sheva");
-
-        try {
-            Thread.sleep(1200);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Finds the state drop down box and chooses value
-        Select State = new Select(ChromeTest.findElement(By.id("id_state")));
-        State.selectByValue("1");
-
-        try {
-            Thread.sleep(1200);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Finds the zipcode box and sends value
-        WebElement ZipCode = ChromeTest.findElement(By.id("postcode"));
-        ZipCode.click();
-        ZipCode.sendKeys("35242");
-
-        try {
-            Thread.sleep(1200);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-
-        // Finds the mobile phone box and sends value
-        WebElement MobilePhone = ChromeTest.findElement(By.id("phone_mobile"));
-        MobilePhone.click();
-        MobilePhone.sendKeys("0535261996");
-
-        try {
-            Thread.sleep(1200);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Finds the alias box and sends value
-        WebElement AddressAlias = ChromeTest.findElement(By.id("alias"));
-        AddressAlias.click();
-        AddressAlias.clear();
-        AddressAlias.sendKeys("Bobos");
-
-
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-
-            ChromeTest.close();
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        ChromeTest.close();
     }
 }
